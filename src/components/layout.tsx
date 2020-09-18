d5d1c8dd8fbbf8f4e855ab4554d99c966ded3421
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2020-09-17 18:54:06 -0600

    Correct import paths

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import loadable from "@loadable/component";
 
 const Home = loadable(() =>
-  import("~components/pages/home").then(({ Home }) => Home)
+  import("~/components/pages/home").then(({ Home }) => Home)
 );
 
 const NotFound = loadable(() =>
-  import("~components/pages/not-found").then(({ NotFound }) => NotFound)
+  import("~/components/pages/not-found").then(({ NotFound }) => NotFound)
 );
 
 export function Layout() {
