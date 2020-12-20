e5c4b78c3f8d812d1b858beb79f489413cef5bea
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2020-12-19 19:08:39 -0700

    Add basic Redux store using RTK

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 
 import { Routes } from "~/components/routes";
+import { store } from "~/store";
 
-render(<Routes />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById("root")
+);
